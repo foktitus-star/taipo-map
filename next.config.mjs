095166e2855ee511b2d1876b3789741d6f75b24f
foktitus-star <foktitus@gmail.com>
2026-05-23 00:52:14 +0800

    404 Debug

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,17 @@ const nextConfig = {
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'X-Frame-Options', value: 'DENY' }
+        ]
+      }
+    ];
+  },
 };
 
 export default nextConfig;
